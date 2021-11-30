e1856b89892436398915297eee77370d8801722b
lupoAI <salomonlupo@gmail.com>
2021-11-30 20:09:52 +0000

    added datagetter

--- data_getter/data_getter.py
+++ data_getter/data_getter.py
@@ -4,7 +4,10 @@ import datetime
 import os
 
 import pandas as pd
+import numpy as np
 import requests
+import datetime as dt
+from tqdm import tqdm
 
 BASE = "https://api.binance.com"
 API_PATHS = {"TIME": BASE + "/api/v3/time",
@@ -20,7 +23,7 @@ API_PATHS = {"TIME": BASE + "/api/v3/time",
              "ORDER_BOOK": BASE + "/api/v3/ticker/bookTicker"}
 
 VALID_INTERVALS = ["1m", "3m", "5m", "15m", "30m", "1h", "2h", "4h", "6h", "8h", "12h", "1d", "3d", "1w", "1M"]
-
+START_HIST = '31/12/2009'
 VALID_INTERVALS_TO_TIME = {"1m": 60 * 1000,
                            "3m": 3 * 60 * 1000,
                            "5m": 5 * 60 * 1000,
@@ -36,7 +39,21 @@ VALID_INTERVALS_TO_TIME = {"1m": 60 * 1000,
                            "3d": 3 * 24 * 60 * 60 * 1000,
                            "1w": 7 * 24 * 60 * 60 * 1000,
                            "1M": 30 * 24 * 60 * 60 * 1000}
-
+INTERVAL_TO_BUCKETS = {"1m": 1,
+                       "3m": 1,
+                       "5m": 1,
+                       "15m": 5,
+                       "30m": 5,
+                       "1h": 10,
+                       "2h": 10,
+                       "4h": 10,
+                       "6h": 10,
+                       "8h": 10,
+                       "12h": 10,
+                       "1d": 10,
+                       "3d": 10,
+                       "1w": 10,
+                       "1M": 10}
 CANDLES_HEADERS = ['open_time', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_asset_volume',
                    'number_of_trades', 'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume', 'ignore']
 
@@ -45,8 +62,6 @@ CANDLES_HEADERS = ['open_time', 'open', 'high', 'low', 'close', 'volume', 'close
 # request
 # REQUEST_WEIGHT =
 
-
-# TODO write tests for these methods
 
 class DataGetter:
 
@@ -60,11 +75,11 @@ class DataGetter:
     def _handle_default_options(self, data, name_file=None):
         if self.pandas:
             res = pd.DataFrame(data)
-            if self.base_save_path is not None:
-                res.to_csv(os.path.join(self.base_save_path, name_file + ".csv"))
+            if (self.base_save_path is not None) and (name_file is not None):
+                res.to_csv(os.path.join(self.base_save_path, name_file + ".csv"), header=False, index=False)
         else:
             res = data
-            if self.base_save_path is not None:
+            if (self.base_save_path is not None) and (name_file is not None):
                 with open(os.path.join(self.base_save_path, name_file + ".p"), 'wb') as handle:
                     pickle.dump(res, handle, protocol=pickle.HIGHEST_PROTOCOL)
         return res
@@ -91,7 +106,7 @@ class DataGetter:
         res = self._handle_request("TIME")
         return res['serverTime']
 
-    def get_exchange_info(self, get_all=False, get_rate_limit=False):
+    def get_exchange_info(self, get_all=False, get_rate_limit=False, save=True):
         """
         refer to https://binance-docs.github.io/apidocs/spot/en/#exchange-information
         """
@@ -103,10 +118,11 @@ class DataGetter:
         file_name = "exchangeInfo" if get_all else f"{self.symbol}_exchangeInfo"
         if get_rate_limit:
             file_name = "rateLimits"
+        file_name = file_name if save else None
 
         return self._handle_default_options(res, file_name)
 
-    def get_partial_book_depth(self, limit: int):
+    def get_partial_book_depth(self, limit: int, save=True):
         """
         refer to https://binance-docs.github.io/apidocs/spot/en/#order-book
         """
@@ -116,11 +132,11 @@ class DataGetter:
 
         params = {"symbol": self.symbol, "limit": limit}
         res = self._handle_request("DEPTH", params=params)
-        file_name = f"{self.symbol}_depth_{self._get_current_time_string()}"
+        file_name = f"{self.symbol}_depth_{self._get_current_time_string()}" if save else None
 
         return self._handle_default_options(res, file_name)
 
-    def get_recent_trades(self, limit: int = 500):
+    def get_recent_trades(self, limit: int = 500, save=True):
         """
         refer to https://binance-docs.github.io/apidocs/spot/en/#recent-trades-list
         """
@@ -129,11 +145,11 @@ class DataGetter:
 
         params = {"symbol": self.symbol, "limit": limit}
         res = self._handle_request("RECENT_TRADES", params=params)
-        file_name = f"{self.symbol}_recentTrades_{self._get_current_time_string()}"
+        file_name = f"{self.symbol}_recentTrades_{self._get_current_time_string()}" if save else None
 
         return self._handle_default_options(res, file_name)
 
-    def get_historical_trades(self, limit: int = 500, from_id: str = None):
+    def get_historical_trades(self, limit: int = 500, from_id: str = None, save=True):
         """
         refer to https://binance-docs.github.io/apidocs/spot/en/#old-trade-lookup-market_data
         """
@@ -144,11 +160,11 @@ class DataGetter:
 
         params = {"symbol": self.symbol, "limit": limit, "fromId": from_id}
         res = self._handle_request("HISTORICAL_TRADES", params=params)
-        file_name = f"{self.symbol}_historicalTrades_{from_id}"
+        file_name = f"{self.symbol}_historicalTrades_{from_id}" if save else None
 
         return self._handle_default_options(res, file_name)
 
-    def get_aggregate_trade_list(self, from_id=None, start_time=None, end_time=None, limit=500):
+    def get_aggregate_trade_list(self, from_id=None, start_time=None, end_time=None, limit=500, save=True):
         """
         refer to https://binance-docs.github.io/apidocs/spot/en/#compressed-aggregate-trades-list
         """
@@ -162,10 +178,10 @@ class DataGetter:
                   "startTime": start_time, "endTime": end_time}
         params = {k: v for k, v in params.items() if v is not None}
         res = self._handle_request("AGGREGATE_TRADES", params=params)
-        file_name = f"{self.symbol}_aggTrades_{from_id}"
+        file_name = f"{self.symbol}_aggTrades_{from_id}" if save else None
         raise self._handle_default_options(res, file_name)
 
-    def get_candles(self, interval, start_time=None, end_time=None, limit=500):
+    def get_candles(self, interval, start_time=None, end_time=None, limit=500, save=True):
         """
         refer to https://binance-docs.github.io/apidocs/spot/en/#kline-candlestick-data
         """
@@ -183,63 +199,140 @@ class DataGetter:
         else:
             file_name = f"{self.symbol}_candles_{interval}_{self._get_current_time_string()}"
 
+        file_name = file_name if save else None
+
         return self._handle_default_options(res, file_name)
 
-    def get_avg_price(self):
+    def get_avg_price(self, save=True):
         """
         refer to https://binance-docs.github.io/apidocs/spot/en/#current-average-price
         """
         params = {"symbol": self.symbol}
         res = self._handle_request("AVG_PRICE", params=params)
-        file_name = f"{self.symbol}_avgPrice_{self._get_current_time_string()}"
+        file_name = f"{self.symbol}_avgPrice_{self._get_current_time_string()}" if save else None
 
         return self._handle_default_options(res, file_name)
 
-    def get_24h_stats(self):
+    def get_24h_stats(self, save=True):
         """
         refer to https://binance-docs.github.io/apidocs/spot/en/#24hr-ticker-price-change-statistics
         """
         params = {"symbol": self.symbol}
         res = self._handle_request("24H", params=params)
-        file_name = f"{self.symbol}_24h_{self._get_current_time_string()}"
+        file_name = f"{self.symbol}_24h_{self._get_current_time_string()}" if save else None
 
         return self._handle_default_options(res, file_name)
 
-    def get_price(self):
+    def get_price(self, save=True):
         """
         refer to https://binance-docs.github.io/apidocs/spot/en/#symbol-price-ticker
         """
         params = {"symbol": self.symbol}
         res = self._handle_request("PRICE", params=params)
-        file_name = f"{self.symbol}_price_{self._get_current_time_string()}"
+        file_name = f"{self.symbol}_price_{self._get_current_time_string()}" if save else None
 
         return self._handle_default_options(res, file_name)
 
-    def get_order_book(self):
+    def get_order_book(self, save=True):
         """
         refer to https://binance-docs.github.io/apidocs/spot/en/#symbol-order-book-ticker
         """
         params = {"symbol": self.symbol}
         res = self._handle_request("ORDER_BOOK", params=params)
-        file_name = f"{self.symbol}_orderBook_{self._get_current_time_string()}"
+        file_name = f"{self.symbol}_orderBook_{self._get_current_time_string()}" if save else None
 
         return self._handle_default_options(res, file_name)
 
+    def get_historical_candles(self, start_date, interval):
+        date_range = pd.date_range(start=start_date, end=dt.date.today(), freq='Y')
+        if date_range[-1].date() != dt.date.today():
+            date_range = date_range.append(pd.Index([pd.Timestamp.now()]))
+        date_range = date_range + dt.timedelta(days=1)
+        date_range_str = date_range.strftime("%Y")[::-1]
+        date_range_milliseconds = list(map(int, (date_range.view('<i8') / 1e6)))[::-1]
+        interval_milliseconds = VALID_INTERVALS_TO_TIME[interval]
+        interval_bucket = INTERVAL_TO_BUCKETS[interval]
+
+        i = 0
+        last = False
+        while True:
+
+            start_ind = min(len(date_range_str) - 1, interval_bucket * (i + 1))
+            if start_ind == len(date_range_str):
+                last = True
+
+            start_date_str = date_range_str[start_ind]
+            end_date_str = date_range_str[interval_bucket * i]
+
+            file_name = f"{self.symbol}_{interval}_{start_date_str}_{end_date_str}"
+            file_path = os.path.join(self.base_save_path, file_name + ".csv")
+            file_path_inc = os.path.join(self.base_save_path, file_name + "_inc.csv")
+            if os.path.exists(file_path) or os.path.exists(file_path_inc):
+                if last:
+                    break
+                i += 1
+                continue
+
+            start_date_m = date_range_milliseconds[start_ind]
+            end_date_m = date_range_milliseconds[interval_bucket * i]
+
+            expected_rows = (end_date_m - start_date_m) / interval_milliseconds ####
+
+            range_m = np.arange(start_date_m, end_date_m, interval_milliseconds * 1000)
+            if len(range_m) == 0:
+                break
+            elif len(range_m) == 1:
+                range_start = [start_date_m]
+                range_end = [end_date_m]
+            else:
+                range_start = range_m[:-1]
+                range_end = range_m[1:] - 1
+                range_start = np.append(range_start, range_m[-1])
+                range_end = np.append(range_end, end_date_m)
+
+            data = pd.DataFrame()
+
+            start_end_range = zip(range_start, range_end)
+            for st, nd in tqdm(start_end_range):
+                try:
+                    temp = self.get_candles(interval, start_time=st, end_time=nd, limit=1000, save=False)
+                    data = data.append(temp)
+                except:
+                    # TODO implement better exception handling
+                    print(f"error for {st} and {nd}")
+
+            if len(data) == 0:
+                break
+
+            start_time_data = data.iloc[0, 0]
+
+            # TODO maybe implement fix that changes start year in file path
+            # start_time_year = pd.to_datetime(data.loc[0,0] * 1e6).strftime("%Y")
+            # if start_time_year != start_date_str:
+
+            if i == 0:
+                file_name += "_inc"
+                file_path = os.path.join(self.base_save_path, file_name + ".csv")
+
+            data.to_csv(file_path, header=False, index=False)
+
+            # We break the loop if we have less rows than expected and we
+            if last or (len(data) < 0.1 * expected_rows) or (
+                    range_start[0] + 10 * interval_milliseconds < start_time_data):
+                print(f"finished for symbol {self.symbol} and interval {interval}")
+                break
+
+            i += 1
+
+    def get_all_historical_candles(self, start_date):
+        for interval in VALID_INTERVALS[::-1]:
+            self.get_historical_candles(start_date, interval)
+            print(f"gotten {interval}")
+
 
 if __name__ == "__main__":
-    import time
-    base_save_path = r"G:\crypto\data\exchange"
-    data_getter = DataGetter(symbol=None, pandas=True, base_save_path=base_save_path)
-    data_getter.get_exchange_info(get_all=True, get_rate_limit=True)
-    # base_save_path = r"G:\crypto\data\symbols"
-    # btcusdt = DataGetter("NULSBTC", pandas=True, base_save_path=base_save_path)
-    # server_time = btcusdt.get_server_time()
-    # interval = "1w"
-    # interval_time = VALID_INTERVALS_TO_TIME[interval]
-    # limit = 1000
-    # end_time = int(time.time() * 1000) - 1#server_time
-    # start_time = end_time - limit * interval_time
-    # for i in range(2):
-    #     data = btcusdt.get_candles(interval, start_time, end_time, limit)
-    #     end_time = start_time
-    #     start_time = end_time - limit * interval_time
+    coins_to_get = ["BTCUSDT", "ETHUSDT", "BUSDUSDT"]
+    base_save_path = r"G:\crypto\data\symbols"
+    for coin in coins_to_get:
+        c = DataGetter(coin, pandas=True, base_save_path=base_save_path)
+        c.get_all_historical_candles(START_HIST)
